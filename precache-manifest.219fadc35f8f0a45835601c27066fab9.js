885c75340e12e44610152e9fa1063d7f845fac3c
maxhha <devmaxhha@gmail.com>
2019-08-18 11:45:21 +0300

    Change project screenshots size

---
+++ precache-manifest.219fadc35f8f0a45835601c27066fab9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be676e683e09f9827ae4ec17c379c070",
+    "url": "https://maxhha.github.io/index.html"
+  },
+  {
+    "revision": "0f2ecf4d2880fe1dcd50",
+    "url": "https://maxhha.github.io/static/css/main.1d6fda5f.chunk.css"
+  },
+  {
+    "revision": "14b31f85cf85feb906b9",
+    "url": "https://maxhha.github.io/static/js/2.8e9af7b4.chunk.js"
+  },
+  {
+    "revision": "0f2ecf4d2880fe1dcd50",
+    "url": "https://maxhha.github.io/static/js/main.3c021f79.chunk.js"
+  },
+  {
+    "revision": "93162d78e2010c5e8a68",
+    "url": "https://maxhha.github.io/static/js/runtime~main.ac9b9cb5.js"
+  }
+]);
